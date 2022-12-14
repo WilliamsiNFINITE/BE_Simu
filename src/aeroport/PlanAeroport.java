8928595a10edaed3eddb6d6efc188beed08ecb69
ThalKod <tmarcelin@thal.tech>
2022-12-14 11:14:43 +0100

    add planaeroport

--- src/aeroport/PlanAeroport.java
+++ src/aeroport/PlanAeroport.java
@@ -1,7 +1,38 @@
 package aeroport;
 
-import engine.Plan;
+import engine.*;
+import enstabretagne.base.time.LogicalDateTime;
+
+import java.util.LinkedList;
 
 public class PlanAeroport extends Plan {
 
+    LogicalDateTime debut;
+    LogicalDateTime fin;
+    LinkedList<ScenarioAeroport> scenarioAeroports;
+
+    public PlanAeroport(SimuEngine engine, int nbReplique, LogicalDateTime debut, LogicalDateTime fin) {
+        super(nbReplique);
+        scenarioAeroports = new LinkedList<>();
+        this.debut = debut;
+        this.fin = fin;
+    }
+
+    @Override
+    public void initScenarii() {
+        for(int i = 0;i < getNbReplique(); i++)
+            scenarioAeroports.add(new ScenarioAeroport(getEngine(), new ScenarioInitData("Scenario 1 ", i, debut, fin), 1));
+    }
+
+    @Override
+    public boolean hasNextScenario() {
+        return scenarioAeroports.size() > 0;
+    }
+
+    @Override
+    public Scenario nextScenario() {
+        Scenario sc = scenarioAeroports.pop();
+        engine.setCurrentScenario(sc);
+        return sc;
+    }
 }
