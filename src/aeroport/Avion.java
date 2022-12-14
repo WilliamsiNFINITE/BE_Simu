2403eefcffc3d5ce6f95016cae68b796cc8ead19
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2022-12-14 09:57:11 +0100

    update Gate and Avion

--- src/aeroport/Avion.java
+++ src/aeroport/Avion.java
@@ -6,7 +6,33 @@ import engine.SimuEngine;
 
 public class Avion extends EntiteSimulee {
 
+    String name;
+    String compagnie;
+    String pays;
+
+
     public Avion(SimuEngine engine, InitData ini) {
         super(engine, ini);
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+    public void setCompagnie(String compagnie) {
+        this.compagnie = compagnie;
+    }
+    public String getCompagnie() {
+        return compagnie;
+    }
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+    public String getPays() {
+        return pays;
+    }
+
 }
--- src/aeroport/Gate.java
+++ src/aeroport/Gate.java
@@ -1,4 +1,14 @@
 package aeroport;
 
 public class Gate {
+
+    String nomGate;
+    Aeroport aeroport;
+    Boolean occupe;
+
+    public Gate(String nomGate, Aeroport aeroport) {
+        this.nomGate = nomGate;
+        this.aeroport = aeroport;
+        this.occupe = false;
+    }
 }
