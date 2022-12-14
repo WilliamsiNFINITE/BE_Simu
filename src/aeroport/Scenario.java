72a33b37f0625421815c7cb59a138f574a0461b8
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2022-12-14 09:30:02 +0100

    add aeroport package

---
+++ src/aeroport/Scenario.java
@@ -0,0 +1,16 @@
+package aeroport;
+
+import engine.ScenarioInitData;
+import engine.SimuEngine;
+
+public class Scenario extends engine.Scenario {
+    public Scenario(SimuEngine engine, ScenarioInitData init) {
+        super(engine, init);
+    }
+
+    @Override
+    public void creerEntitesSimulees() {
+
+    }
+}
+
